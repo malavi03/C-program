9eb2ff7cc9a17e701b9fd0cfb312af5684418e42
Malavi <malavisenthil2007@gmail.com>
2026-07-22 23:11:54 +0530

    Added symbolic.c

---
+++ symbolic.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#define MAX_MARKS 100
+int main(){
+printf("The maximum marks has %d",MAX_MARKS);
+//EXPLANATION
+printf("the symbolic constant cannot be modified during execution");
+return 0;
+}
