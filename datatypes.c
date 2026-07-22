1bab56196c2b04f721e5490fdac83e5dd44732f7
Malavi <malavisenthil2007@gmail.com>
2026-07-22 23:02:41 +0530

    Added datatypes.c

---
+++ datatypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+int a=3;
+float b=3.14;
+char c='a';
+double  d=3.1424;
+printf("%d\n  ",a);
+printf("%f\n  ",b);
+printf("%c\n  ",c);
+printf("%f\n  ",d);
+return 0;
+}
