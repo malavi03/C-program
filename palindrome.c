3fe9b4c2af9cd56d94df33de7dd41f8910851588
Malavi <malavisenthil2007@gmail.com>
2026-07-26 14:52:44 +0530

    Added palindrome program

---
+++ palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+int rev=0,digit,n,temp;
+printf("Enter the number");
+scanf("%d",&n);
+temp=n;
+while(n!=0){
+digit=n%10;
+rev=(rev*10)+digit;
+n=n/10;
+}
+//printf("Reverse number : %d",rev);
+if(rev==temp){
+printf("palindrome");
+}
+else{
+printf("Not palindrome");
+}
+
+
+return 0;
+}
+
+
